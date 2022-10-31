df30338864fc4c0ab6451a06a787e2780a3eefb2
Banasik Krzysztof <krzysztof.f.banasik@gmail.com>
2022-11-01 00:44:17 +0100

    Improve the GUI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,15 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {HttpClientJsonpModule, HttpClientModule} from '@angular/common/http';
 import {ReactiveFormsModule} from '@angular/forms';
-
 import {AppComponent} from './app.component';
 import {TopBarComponent} from './top-bar/top-bar.component';
-import {BottomBarComponent} from './bottom-bar/bottom-bar.component';
 import {RecommenderComponent} from './recommender/recommender.component';
 import {MapViewComponent} from './map-view/map-view.component';
 import {GoogleMapsModule} from "@angular/google-maps";
 import {NgxsModule} from "@ngxs/store";
 import {RecommenderState} from "./recommender/recommender.state";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {InfoModalComponent} from "./info-modal/info-modal.component";
 
 @NgModule({
   imports: [
@@ -22,15 +22,16 @@ import {RecommenderState} from "./recommender/recommender.state";
     NgxsModule.forRoot(
       [RecommenderState],
       {selectorOptions: {suppressErrors: true}}
-    )
+    ),
+    NgbModule
   ],
   declarations: [
     AppComponent,
     TopBarComponent,
-    BottomBarComponent,
     RecommenderComponent,
     MapViewComponent,
-    RecommenderComponent
+    RecommenderComponent,
+    InfoModalComponent
   ],
   bootstrap: [
     AppComponent
--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -1,4 +1,6 @@
 import {Component} from '@angular/core';
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import {InfoModalComponent} from "../info-modal/info-modal.component";
 
 @Component({
   selector: 'app-top-bar',
@@ -6,7 +8,14 @@ import {Component} from '@angular/core';
   styleUrls: ['./top-bar.component.css']
 })
 export class TopBarComponent {
+
+  constructor(private modalService: NgbModal) {}
+
   refresh() {
     window.location.reload();
+  }
+
+  openDialog() {
+    this.modalService.open(InfoModalComponent)
   }
 }
